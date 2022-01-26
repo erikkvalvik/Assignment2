7e5a04c18dc6555573c41ac8b40ef7dc6dc4aa1c
Erik Kvalvik <erik.kvalvik@no.experis.com>
2022-01-26 16:34:23 +0100

    Added ConnectionHelper.cs

---
+++ Assignment2/Repositories/ConnectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Repositories
+{
+    class ConnectionHelper
+    {
+        public static string GetConnectionstring()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "ND-5CG9030M8H\\SQLEXPRESS";
+            connectionStringBuilder.InitialCatalog = "Chinook";
+            connectionStringBuilder.IntegratedSecurity = true;
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
