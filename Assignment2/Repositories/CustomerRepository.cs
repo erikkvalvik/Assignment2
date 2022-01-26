13cfd15a583a9947a6852d5efdc89abeb56de777
Erik Kvalvik <erik.kvalvik@no.experis.com>
2022-01-26 17:59:48 +0100

    Added Customer model with add, update and multiple read functions

---
+++ Assignment2/Repositories/CustomerRepository.cs
@@ -0,0 +1,208 @@
+﻿using Assignment2.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Repositories
+{
+    class CustomerRepository : ICustomerRepository
+    {
+        /// <summary>
+        /// Adds new customer to database. Checks if query executes successfully.
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        public bool AddNewCustomer(Customer customer)
+        {
+            bool success = false;
+            string sql = "INSERT INTO Customer(CustomerId, FirstName, LastName, Country, PostalCode, " +
+                "Phone, Email) VALUES(@CustomerId, @FirstName, @LastName, @Country, @PostalCode, " +
+                "@Phone, @Email)";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionHelper.GetConnectionstring()))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerID);
+                        cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                        cmd.Parameters.AddWithValue("@Country", customer.Country);
+                        cmd.Parameters.AddWithValue("@PostalCode", customer.PostalCode);
+                        cmd.Parameters.AddWithValue("@Phone", customer.PhoneNumber);
+                        cmd.Parameters.AddWithValue("@Email", customer.Email);
+                        success = cmd.ExecuteNonQuery() > 0 ? true : false;
+                    }
+                }
+            }
+            catch(SqlException ex)
+            {
+                //Log to console
+            }
+            return success;
+        }
+
+        public bool DeleteCustomer(string id)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Reads and displays ID, First and Last name, Country, Postal code, phone number
+        /// and Email of all the customers in the database.Returns them as a list.
+        /// </summary>
+        /// <returns></returns>
+        public List<Customer> GetAllCustomers()
+        {
+            List<Customer> customerList = new List<Customer>();
+            string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email" +
+                " FROM Customer";
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(ConnectionHelper.GetConnectionstring()))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Customer temp = new Customer();
+                                temp.CustomerID = reader.GetString(0);
+                                temp.FirstName = reader.GetString(1);
+                                temp.LastName = reader.GetString(2);
+                                temp.Country = reader.GetString(7);
+                                temp.PostalCode = reader.GetString(8);
+                                temp.PhoneNumber = reader.GetString(9);
+                                temp.Email = reader.GetString(11);
+                                customerList.Add(temp);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(SqlException ex)
+            {
+                //Log to console
+            }
+            return customerList;
+            
+        }
+        /// <summary>
+        /// Reads a specific customer by its Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Customer GetCustomerById(string id)
+        {
+            Customer customer = new Customer();
+            string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email" +
+                " FROM Customer WHERE CustomerId = @CustomerId";
+            try
+            {
+                using(SqlConnection conn = new SqlConnection(ConnectionHelper.GetConnectionstring()))
+                {
+                    conn.Open();
+                    using(SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@CustomerId", id);
+                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                customer.CustomerID = reader.GetString(0);
+                                customer.FirstName = reader.GetString(1);
+                                customer.LastName = reader.GetString(2);
+                                customer.Country = reader.GetString(7);
+                                customer.PostalCode = reader.GetString(8);
+                                customer.PhoneNumber = reader.GetString(9);
+                                customer.Email = reader.GetString(11);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                
+                //Log to console
+            }
+            return customer;
+        }
+        /// <summary>
+        /// Reads a specific customer by its name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Customer GetCustomerByName(string name)
+        {
+            Customer customer = new Customer();
+            string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email" +
+                "  FROM Customer WHERE FirstName LIKE @FirstName";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionHelper.GetConnectionstring()))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@FirstName", name);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                customer.CustomerID = reader.GetString(0);
+                                customer.FirstName = reader.GetString(1);
+                                customer.LastName = reader.GetString(2);
+                                customer.Country = reader.GetString(7);
+                                customer.PostalCode = reader.GetString(8);
+                                customer.PhoneNumber = reader.GetString(9);
+                                customer.Email = reader.GetString(11);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                //Log to error
+            }
+            return customer;
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            bool success = false;
+            string sql = "UPDATE Customer SET CustomerId = @CustomerId, FirstName = @FirstName, " +
+                "LastName = @LastName, Country = @Country, PostalCode = @PostalCode, " +
+                "Phone = @Phone, Email = @Email";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionHelper.GetConnectionstring()))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerID);
+                        cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                        cmd.Parameters.AddWithValue("@Country", customer.Country);
+                        cmd.Parameters.AddWithValue("@PostalCode", customer.PostalCode);
+                        cmd.Parameters.AddWithValue("@Phone", customer.PhoneNumber);
+                        cmd.Parameters.AddWithValue("@Email", customer.Email);
+                        success = cmd.ExecuteNonQuery() > 0 ? true : false;
+                    }
+                }
+            }
+            catch(SqlException ex)
+            {
+                //Log to console
+            }
+            return success;
+        }
+    }
+}
---
+++ Assignment2/Repositories/ICustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Repositories
+{
+    public interface ICustomerRepository
+    {
+        public Customer GetCustomerById(string id);
+        public Customer GetCustomerByName(string name);
+        public List<Customer> GetAllCustomers();
+        public bool AddNewCustomer(Customer customer);
+        public bool UpdateCustomer(Customer customer);
+        public bool DeleteCustomer(string id);
+
+        //Return a page of customers from the database. SHould take in limit and offset as parameters.
+
+        //Return number of customers in each counry, desc
+
+        //Return highest spenders, ordered descending
+
+        //Return given customers most popular genre, display both if tie
+        //(most tracks from invoices associated to that customer)
+    }
+}
